7c053848bd689e1bd04fee2e418ef400e6ccd4b9
angelog <gabriel_1997.mc@hotmail.com>
2019-10-03 17:37:44 -0300

    Codigo atualizado

---
+++ PI atualizado/projeto.py
@@ -0,0 +1,19 @@
+import requests
+url = 'https://dadosabertos.camara.leg.br/api/v2/deputados'
+resp = requests.get(url).json()
+# file = open('id_dep.txt', 'w')
+# for d in resp['dados']:
+#     a = (str(d['id']) + "\n")
+#     file.writelines(a)
+
+for d in resp['dados']:
+    nome_deputado = str(d['nome'])
+    id_deputado = str(d['id'])
+    url = 'https://dadosabertos.camara.leg.br/api/v2/deputados/' + id_deputado + '/despesas?ano=2019&mes=09&ordem=ASC&ordenarPor=ano'
+    post = requests.get(url).json()
+    print(post)
+    file = open('despesas' + nome_deputado + '_' + id_deputado + '.txt', 'w')
+
+    for d in post ['dados']:
+         b = (d['tipoDespesa'] + " = " + str(d['valorDocumento']) + "\n")
+         file.writelines(b)
