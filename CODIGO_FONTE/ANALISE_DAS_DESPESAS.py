001cab68ce8351f3e01d2ea1298d0f797f2c629f
n4htan <n4htan@hotmail.com>
2019-10-21 20:58:36 -0300

    configurando tabela dos maiores

--- CODIGO_FONTE/ANALISE_DAS_DESPESAS.py
+++ CODIGO_FONTE/ANALISE_DAS_DESPESAS.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-PLANILHA = pd.read_csv("NOME_ID_TOTAL.csv", encoding='latin-1', index_col="NOME")
+PLANILHA = pd.read_csv("NOME_ID_TOTAL.csv", encoding='latin-1')
 
-#ORDENAR DADOS DA TABELA ATRAVÉS DA FUNÇÃO "sort_values"
+# ORDENAR DADOS DA TABELA ATRAVÉS DA FUNÇÃO "sort_values"
 
 # ORDENANDO DECRESCENTE
 tabela_maior = PLANILHA.sort_values(['GASTO','ID'], ascending=False)
@@ -14,3 +15,13 @@ tabela_menor = PLANILHA.sort_values(['GASTO','ID'])
 tabela_menor[['GASTO','ID']].head(10)
 print(tabela_menor)
 
+
+# PLOTANDO OS 10 MAIS
+plt.rcParams['xtick.labelsize'] = 8
+plt.title('OS 10 DEPUTADOS QUE MAIS GASTARAM EM 09/2019')
+x = (tabela_maior['NOME'])
+plt.xlabel('NOME DO DEPUTADO')
+y = (tabela_maior['GASTO'])
+plt.ylabel('GASTOS EM R$')
+plt.bar(x, y, color="black")
+plt.show()
