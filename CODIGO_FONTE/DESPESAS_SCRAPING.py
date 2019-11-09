5a3efa53ef841a8d1db765d824b4f763a37d0698
n4htan <n4htan@hotmail.com>
2019-11-09 13:56:51 -0300

    busca dos meses

--- CODIGO_FONTE/DESPESAS_SCRAPING.py
+++ CODIGO_FONTE/DESPESAS_SCRAPING.py
@@ -1,34 +1,44 @@
 import requests
 
-url = 'https://dadosabertos.camara.leg.br/api/v2/deputados'
-resp = requests.get(url).json()
+URL = 'https://dadosabertos.camara.leg.br/api/v2/deputados'
+RESP = requests.get(URL).json()
 
-file_csv = open('NOME_ID_TOTAL.csv', 'w')  # criando arquivo csv para escrever nome, ID, e total de gastos de cada deputado
+FILE_CSV = open('NOME_ID_TOTAL.csv', 'w')  # criando arquivo csv para escrever nome, ID, e total de gastos de cada deputado
 
-for d in resp['dados']: # for usado para buscar nome, id e as diversas despesas de cada deputado
-    nome_deputado = str(d['nome'])
-    id_deputado = str(d['id'])
-    partido_deputado = str(d['siglaPartido'])
-    UF_deputado = str(d['siglaUf'])
-    url = 'https://dadosabertos.camara.leg.br/api/v2/deputados/' + id_deputado + '/despesas?ano=2019&mes=09&ordem=ASC&ordenarPor=ano'
-    post = requests.get(url).json()
-    print(post)
+CONTADOR = 2
+GUIA_SOMA = 2
+SOM = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# criando arquivo txt para escrever as diversas despesas de cada deputado
-    file = open('DESPESAS_' + nome_deputado + '_' + id_deputado + '_' + partido_deputado + '_' + UF_deputado + '.txt', 'w')
+while CONTADOR <= 9:
 
+    for INFO in RESP['dados']: # for usado para buscar nome, id e as diversas despesas de cada deputado
+        NOME_DEPUTADO = str(INFO['nome'])
+        ID_DEPUTADO = str(INFO['id'])
+        PARTIDO_DEPUTADO = str(INFO['siglaPartido'])
+        UF_DEPUTADO = str(INFO['siglaUf'])
+        URL = 'https://dadosabertos.camara.leg.br/api/v2/deputados/' + ID_DEPUTADO + '/despesas?ano=2019&mes=0' + str(GUIA_SOMA) + '&ordem=ASC&ordenarPor=ano'
+        POST = requests.get(URL).json()
+        print(POST)
 
-    soma = 0.0
+    # criando arquivo txt para escrever as diversas despesas de cada deputado
+ #       FILE_TXT = open('DESPESAS_' + NOME_DEPUTADO + '_' + ID_DEPUTADO + '_' + PARTIDO_DEPUTADO + '_' + UF_DEPUTADO + '.txt', 'w')
 
-    for d in post['dados']:
-        b = (d['tipoDespesa'] + " = " + str(d['valorDocumento']) + "   (" + str(d['dataDocumento']) + ")\n")
-        file.writelines(b)      # escrevendo as despesas no arquivo txt
+        SOMA = 0.0
 
-        t = float(d['valorDocumento'])
-        soma = soma + t
-        som = str("%.2f" % soma)
-    file.writelines("TOTAL DE DESPESAS = " + som + '\n' "PARTIDO DEPUTADO = " + partido_deputado + '\n' "UF DEPUTADO = " + UF_deputado)    #escrevendo o total de despesas no arquivo txt
-    linha_csv = (nome_deputado + ';' + id_deputado  + ';' + som + ';' + partido_deputado + ';' + UF_deputado + '\n')
-    file_csv.writelines(linha_csv)     # escrevendo nome, ID e total de despesas de cada deputado no arquivo csv
+        for INFO in POST['dados']:
+#            DESPESAS_TXT = (INFO['tipoDespesa'] + " = " + str(INFO['valorDocumento']) + "   (" + str(INFO['dataDocumento']) + ")\n")
+#            FILE_TXT.writelines(DESPESAS_TXT)      # escrevendo as despesas no arquivo txt
 
+            VALOR_DOC = float(INFO['valorDocumento'])
+            SOMA = SOMA + VALOR_DOC
+            SOMA = str("%.2f" %SOMA)
+            SOMA = float(SOMA)
+            CONTADOR = int(CONTADOR)
+            SOM[GUIA_SOMA] = SOMA
+#        FILE_TXT.writelines("TOTAL DE DESPESAS = " + SOM + '\n' "PARTIDO DEPUTADO = " + PARTIDO_DEPUTADO + '\n' "UF DEPUTADO = " + UF_DEPUTADO)    #escrevendo o total de despesas no arquivo txt
+        LINHA_CSV = (NOME_DEPUTADO + ';' + ID_DEPUTADO  + ';' + PARTIDO_DEPUTADO + ';' + UF_DEPUTADO + ';' + str(SOM[2]) + ';' + str(SOM[3]) + ';' + str(SOM[4]) + ';' + str(SOM[5]) + ';' + str(SOM[6]) + ';' + str(SOM[7]) + ';' + str(SOM[8]) + ';' + str(SOM[9]) + '\n')
+        FILE_CSV.writelines(LINHA_CSV)     # escrevendo nome, ID e total de despesas de cada deputado no arquivo csv
+
+    CONTADOR = CONTADOR + 1
+    GUIA_SOMA = GUIA_SOMA + 1
 
