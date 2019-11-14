dba055ed7ac70ca7887db21d1a205a4ca5410a62
Gabriel <eeeesilva@gmail.com>
2019-11-14 01:51:23 +0000

    adicionando dicionario para organização do csv

--- CODIGO_FONTE/DESPESAS_SCRAPING.py
+++ CODIGO_FONTE/DESPESAS_SCRAPING.py
@@ -8,6 +8,7 @@ FILE_CSV = open('NOME_ID_TOTAL.csv', 'w')  # criando arquivo csv para escrever n
 CONTADOR = 2
 GUIA_SOMA = 2
 SOM = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+politicos = {}
 
 while CONTADOR <= 9:
 
@@ -21,7 +22,7 @@ while CONTADOR <= 9:
         print(POST)
 
     # criando arquivo txt para escrever as diversas despesas de cada deputado
- #       FILE_TXT = open('DESPESAS_' + NOME_DEPUTADO + '_' + ID_DEPUTADO + '_' + PARTIDO_DEPUTADO + '_' + UF_DEPUTADO + '.txt', 'w')
+    #     FILE_TXT = open('DESPESAS_' + NOME_DEPUTADO + '_' + ID_DEPUTADO + '_' + PARTIDO_DEPUTADO + '_' + UF_DEPUTADO + '.txt', 'w')
 
         SOMA = 0.0
 
@@ -31,14 +32,23 @@ while CONTADOR <= 9:
 
             VALOR_DOC = float(INFO['valorDocumento'])
             SOMA = SOMA + VALOR_DOC
-            SOMA = str("%.2f" %SOMA)
-            SOMA = float(SOMA)
+            SOMA = float("%.2f" %SOMA)
             CONTADOR = int(CONTADOR)
-            SOM[GUIA_SOMA] = SOMA
 #        FILE_TXT.writelines("TOTAL DE DESPESAS = " + SOM + '\n' "PARTIDO DEPUTADO = " + PARTIDO_DEPUTADO + '\n' "UF DEPUTADO = " + UF_DEPUTADO)    #escrevendo o total de despesas no arquivo txt
-        LINHA_CSV = (NOME_DEPUTADO + ';' + ID_DEPUTADO  + ';' + PARTIDO_DEPUTADO + ';' + UF_DEPUTADO + ';' + str(SOM[2]) + ';' + str(SOM[3]) + ';' + str(SOM[4]) + ';' + str(SOM[5]) + ';' + str(SOM[6]) + ';' + str(SOM[7]) + ';' + str(SOM[8]) + ';' + str(SOM[9]) + '\n')
-        FILE_CSV.writelines(LINHA_CSV)     # escrevendo nome, ID e total de despesas de cada deputado no arquivo csv
 
+        if NOME_DEPUTADO in politicos:
+            politicos[NOME_DEPUTADO].append(SOMA)
+        else:
+            politicos[NOME_DEPUTADO] = [NOME_DEPUTADO, ID_DEPUTADO, PARTIDO_DEPUTADO, UF_DEPUTADO, SOMA]
+
+#        LINHA_CSV = (NOME_DEPUTADO + ';' + ID_DEPUTADO  + ';' + PARTIDO_DEPUTADO + ';' + UF_DEPUTADO + ';' + str(SOM[2]) + ';' + str(SOM[3]) + '\n')
+#        FILE_CSV.writelines(LINHA_CSV)     # escrevendo nome, ID e total de despesas de cada deputado no arquivo csv
+#    print(politicos)
     CONTADOR = CONTADOR + 1
     GUIA_SOMA = GUIA_SOMA + 1
+
+    for politico in politicos:
+        dados = (';'.join(map(str, politicos[politico])))
+        print(dados)
+    FILE_CSV.writelines(dados)
 
